a7a81b47f0db760ecdef6ec9960e27222f1d0794
Stefanos Koutsouflakis <koutsouflakis.stefanos@gmail.com>
2020-12-19 05:19:36 +0200

    first commit

---
+++ gof.js
@@ -0,0 +1,13 @@
+var a = 10;
+var b = 10;
+var resultTable = '<table cellspacing="0" cellpadding="0">';
+
+for (var i = 0; i < parseInt(a); i++) {
+    resultTable += '<tr>';
+    for (var j = 0; j < parseInt(b); j++)
+        resultTable += '<td><input type="checkbox" class="regular-checkbox"><\/td>';
+}
+resultTable += '<\/tr>';
+resultTable += '<\/table>';
+var target = document.getElementById('myElement');
+target.innerHTML = resultTable;
