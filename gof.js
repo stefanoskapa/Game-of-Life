35135acdbdbfa3f789deafb45944597f1be86536
Stefanos Koutsouflakis <koutsouflakis.stefanos@gmail.com>
2020-12-19 16:41:13 +0200

    good

--- gof.js
+++ gof.js
@@ -1,9 +1,8 @@
-var a = 50;
-var b = 50;
+const a = 200;
 var resultTable = '<table name = "mytab" cellspacing="0" cellpadding="0">';
 for (var i = 0; i < parseInt(a); i++) {
     resultTable += '<tr>';
-    for (var j = 0; j < parseInt(b); j++) {
+    for (var j = 0; j < parseInt(a); j++) {
     var uid = i+'.'+j;
         resultTable += '<td class="container"><input type="checkbox" id="'+uid+'" class="regular-checkbox"><\/td>';
     }
@@ -12,59 +11,41 @@ resultTable += '<\/tr>';
 resultTable += '<\/table>';
 var target = document.getElementById('myElement');
 target.innerHTML = resultTable;
+
 function aliveNeigh(ida) {
     var count=0;
     var coordinates = ida.split(".");
-   /* alert(parseInt(coordinates[0]) + '.' + coordinates[1]);*/
     var aaa = parseInt(coordinates[0]);
-    
     var bbb = parseInt(coordinates[1]);
-    /*alert(aMin+'.'+ aPlu + '.' + bMin+ '.' +bPlu);*/
-
-    if (aaa-1 >=0 && bbb-1 >=0) {
-        if (document.getElementById((aaa - 1) + '.' + (bbb - 1)).checked) count++;        
-    }
-    if (aaa-1 >=0) {
-        if (document.getElementById((aaa - 1) + '.' + bbb).checked) count++;
-    }
-    if (aaa-1 >=0 && bbb+1 <=49) {
-        if (document.getElementById((aaa - 1) + '.' + (bbb + 1)).checked) count++;
-    }
-    if (bbb-1 >=0) {
-        if (document.getElementById(aaa + '.' + (bbb - 1)).checked) count++;
-    }
-    if (bbb+1 <=49) {
-        if (document.getElementById(aaa + '.' + (bbb + 1)).checked) count++;
-    }
-    if (aaa+1 <=49 && bbb-1 >=0) {
-        if (document.getElementById((aaa + 1) +'.' + (bbb - 1)).checked) count++;
-    }
-    if (aaa+1 <=49) {
-        if (document.getElementById((aaa + 1) + '.' + bbb).checked) count++;
-    }
-    if (aaa+1 <=49 && bbb+1 <=49) {
-        if (document.getElementById((aaa + 1) + '.' + (bbb + 1)).checked) count++;
-    }
+    if ((aaa-1 >=0 && bbb-1 >=0) && (document.getElementById((aaa - 1) + '.' + (bbb - 1)).checked)) count++;        
+    if ((aaa-1 >=0 && document.getElementById((aaa - 1) + '.' + bbb).checked)) count++;
+    if ((aaa-1 >=0 && bbb+1<=a-1) && (document.getElementById((aaa - 1) + '.' + (bbb + 1)).checked)) count++;
+    if ((bbb-1 >=0 && document.getElementById(aaa + '.' + (bbb - 1)).checked)) count++;
+    if ((bbb+1 <= a-1 && document.getElementById(aaa + '.' + (bbb + 1)).checked)) count++;
+    if ((aaa+1 <=a-1 && bbb-1 >=0) && (document.getElementById((aaa + 1) +'.' + (bbb - 1)).checked)) count++;
+    if ((aaa+1 <=a-1 && document.getElementById((aaa + 1) + '.' + bbb).checked)) count++;
+    if ((aaa+1 <=a-1 && bbb+1<=a-1) && (document.getElementById((aaa + 1) + '.' + (bbb + 1)).checked)) count++;
+    
     return count;
-    /*alert(count);*/
 }
-function startGame() {
-    var c = 50;
-    var d = 50;
+
+function play(){    
+        setInterval(nextGen, 300);
+}
+function nextGen() {
+    
     var storage = new Array(250);
     counta = 0;
-    for (var i = 0; i < parseInt(c); i++) {
-        
-        for (var j = 0; j < parseInt(d); j++) {
+    for (var i = 0; i < parseInt(a); i++) {       
+        for (var j = 0; j < parseInt(a); j++) {
         var yy = i + '.' + j;
         storage[counta] = aliveNeigh(yy);
         counta++;
         }
     }
     counta = 0
-    for (var i = 0; i < parseInt(c); i++) {
-        
-        for (var j = 0; j < parseInt(d); j++) {
+    for (var i = 0; i < parseInt(a); i++) {
+        for (var j = 0; j < parseInt(a); j++) {
         var yy = i + '.' + j;
         if (storage[counta]<2) {           
             document.getElementById(yy).checked=false;
@@ -76,7 +57,6 @@ function startGame() {
             document.getElementById(yy).checked=false;;
         }
             counta++;
-            
         }
         }
     }
